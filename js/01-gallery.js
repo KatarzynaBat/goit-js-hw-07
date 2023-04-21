3d2f022f9d88e068e7d2b10d2e9b3e917154082e
KatarzynaBat <117004275+KatarzynaBat@users.noreply.github.com>
2023-04-21 15:45:57 +0200

    hw7

---
+++ js/01-gallery.js
@@ -0,0 +1,37 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector(".gallery")
+
+for (const image of galleryItems)
+{
+    const galleryElement = 
+   ` <li>
+<div class="gallery__item">
+<a class="gallery__link" href= "${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    data-source=${image.original}"
+    alt="${image.description}"
+  />
+</a>
+</div>
+</li>`
+gallery.innerHTML +=  galleryElement
+
+}
+
+gallery.addEventListener('click', event => {
+    event.preventDefault();
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`, {
+      onShow: instance => {
+        window.addEventListener('keydown', event => {
+          if (event.key === 'Escape') {
+            instance.close();
+          }
+        });
+      },
+    });
+    instance.show();
+  });
---
+++ js/02-lightbox.js
@@ -0,0 +1,22 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector(".gallery")
+
+for (const image of galleryItems)
+{
+    const galleryElement = 
+   ` <li>
+<a class="gallery__item" href= "${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    
+    alt="" title =${image.description}"
+  />
+</a>
+</li>`
+gallery.innerHTML +=  galleryElement
+}
+const lightbox = new SimpleLightbox('.gallery li a')
+console.log(galleryItems);
